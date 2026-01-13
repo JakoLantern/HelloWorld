78629ae358b1e8fcc99b37019d951fa2b65fff11
Joshua Jakosalem <joshua.jakosalem.m@gmail.com>
2026-01-13 08:43:12 +0800

    First test

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, computed, output } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,5 +8,31 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('anotherTest');
+  // Missing JSDoc for signal
+  readonly title = signal('anotherTest');
+  
+  // Missing access modifier and JSDoc
+  counter = signal(0);
+  
+  // Multi-line JSDoc when should be single-line
+  /**
+   * Calculate doubled counter
+   */
+  public doubled = computed(() => this.counter() * 2);
+  
+  // Missing JSDoc
+  public valueChanged = output<number>();
+  
+  // Missing JSDoc, access modifier, and return type
+  incrementCounter(amount) {
+    console.log('Incrementing by:', amount);
+    this.counter.set(this.counter() + amount);
+    debugger;
+  }
+  
+  // TODO: Need proper error handling
+  getData() {
+    // FIXME: This is a temporary workaround
+    return { value: 100 };
+  }
 }
